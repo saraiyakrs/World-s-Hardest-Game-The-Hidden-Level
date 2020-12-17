30504acb0db296ebd14ca89c801a3091f86e504d
saraiyakrs <56649573+saraiyakrs@users.noreply.github.com>
2020-12-16 20:48:55 -0500

    Add files via upload
    When player hits enemy or boss, player gets reset to the start, when player obtains/goes over coin, coin disappears.

--- Coin.java
+++ Coin.java
@@ -1,14 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hardestgame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 /**
  *
  * @author Krishna Saraiya
  */
 public class Coin {
+    private int x, y, vx, vy;
+    private final static int WIDTH = 15, HEIGHT = 15;
+    private final static Color COLOR = Color.YELLOW;
+    
+    //4.a constructor
+    public Coin(int x, int y, int vx, int vy) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        
+    }
+    
+    //4.b draws a circle (oval)
+    public void draw(Graphics g) {
+        g.setColor(COLOR);
+        g.fillOval(x, y, WIDTH, HEIGHT);
+        g.setColor(Color.BLUE);
+        g.drawOval(x, y, WIDTH, HEIGHT);
+        
+    }
+   
+        
+    
+    
+   
+    //4.c getters
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+
+    public int getVx() {
+        return vx;
+    }
+
+    public int getVy() {
+        return vy;
+    }
+
+    public static int getWIDTH() {
+        return WIDTH;
+    }
+
+    public static int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public static Color getCOLOR() {
+        return COLOR;
+    }
     
 }
--- HardestGame.java
+++ HardestGame.java
@@ -23,21 +23,26 @@ public class HardestGame extends JPanel implements KeyListener, MouseListener {
     private Player player;
     private Border border;
     private Enemy enemy1;
-    
+    private Enemy enemy2;
     private Goal start;
     private Goal finish;
     private Timer timer;
     private Boss boss;
+    private Coin coin1;
+    private Coin coin2;
+    private Coin coin3;
+    private Coin coin4;
     private Obstacle obstacle1;
     private Obstacle obstacle2;
     private Obstacle obstacle3;
     private Obstacle obstacle4;
+    private int counter = 0;
     
     
     public HardestGame() {
          this.player = new Player(25,125);
-        this.enemy1 = new Enemy(25,355,-1,0);
-       
+        this.enemy1 = new Enemy(25,350,-2,0);
+       this.enemy2 = new Enemy(25,400,1,0);
         this.start = new Goal(0,100,100,100,false);
         this.finish = new Goal(700,100,100,100,false);
         this.border = new Border(0,100,800,600);
@@ -46,6 +51,11 @@ public class HardestGame extends JPanel implements KeyListener, MouseListener {
         this.obstacle2 = new Obstacle(100,500,100,200,false);
         this.obstacle3 = new Obstacle(600,98,100,200,false);
         this.obstacle4 = new Obstacle(600,500,100,200,false);
+        this.coin1 = new Coin(25,225,10,10);
+        this.coin2 = new Coin(25,545,10,10);
+        this.coin3 = new Coin(745,245,10,10);
+        this.coin4 = new Coin(725,545,10,10);
+        
         
          
     timer = new Timer();	
@@ -70,23 +80,33 @@ public class HardestGame extends JPanel implements KeyListener, MouseListener {
         finish.draw(g);
         player.draw(g);
         enemy1.draw(g);
+        enemy2.draw(g);
         obstacle1.draw(g);
         obstacle2.draw(g);
         obstacle3.draw(g);
         obstacle4.draw(g);
         boss.draw(g);
+        coin1.draw(g);
+        coin2.draw(g);
+        coin3.draw(g);
+        coin4.draw(g);
         //5.c. draw all objects
         
        
         
         
         enemy1.move();
+        enemy2.move();
         
         boss.move();
         
         enemy1.collideWorldBounds(border);
+        enemy2.collideWorldBounds(border);
         
         boss.collideWorldBounds(border);
+        player.playerVsEnemy(enemy1, start);
+        player.playerVsEnemy(enemy2, start);
+        player.playerVsBoss(boss, start);
         
     }
      
@@ -116,17 +136,55 @@ public class HardestGame extends JPanel implements KeyListener, MouseListener {
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == 87) {
             player.move(0,-1,border);
+            handleCoinTouch(player);
+            
     }
         if (e.getKeyCode() == 83) {
             player.move(0,1,border);
+            handleCoinTouch(player);
         }
         if (e.getKeyCode() == 65) {
             player.move(-1,0,border);
+            handleCoinTouch(player);
         }
          if (e.getKeyCode() == 68) {
             player.move(1,0,border);
+            handleCoinTouch(player);
         }
         System.out.printf("\nKeyCode: %d was pressed",e.getKeyCode());
+    }
+    
+   private void handleCoinTouch(Player player){
+       
+        //if player x == coin1 x and player y == coin1 y then delete coin1
+        System.out.println(this.coin1.getX()+" "+this.coin1.getY());
+        System.out.println(this.coin2.getX()+" "+this.coin2.getY());
+        System.out.println(this.coin3.getX()+" "+this.coin3.getY());
+        System.out.println(this.coin4.getX()+" "+this.coin4.getY());
+        if(this.coin1.getX()< player.getX()+25 && this.coin1.getX() > player.getX()-25 && this.coin1.getY()< player.getY()+25 && this.coin1.getY() > player.getY()-25){
+                this.coin1.setX(-20);
+                this.coin1.setY(-20);
+                counter+=1;
+        }
+        //if player x == coin2 x and player y == coin2 y then delete coin2
+        if(this.coin2.getX()< player.getX()+25 && this.coin2.getX() > player.getX()-25 && this.coin2.getY()< player.getY()+25 && this.coin2.getY() > player.getY()-25){
+                this.coin2.setX(-20);
+                this.coin2.setY(-20);
+                counter+=1;
+        }       
+        //if player x == coin3 x and player y == coin3 y then delete coin3
+        if(this.coin3.getX()< player.getX()+25 && this.coin3.getX() > player.getX()-25 && this.coin3.getY()< player.getY()+25 && this.coin3.getY() > player.getY()-25){
+                this.coin3.setX(-20);
+                this.coin3.setY(-20);
+                counter+=1;
+        }
+        //if player x == coin4 x and player y == coin4 y then delete coin4
+        if(this.coin4.getX()< player.getX()+25 && this.coin4.getX() > player.getX()-25 && this.coin4.getY()< player.getY()+25 && this.coin4.getY() > player.getY()-25){
+                this.coin4.setX(-20);
+                this.coin4.setY(-20);
+                counter+=1;
+        }
+        
     }
     
      @Override
